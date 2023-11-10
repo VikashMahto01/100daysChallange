8eb23550df386726378cef195615aed5bd47b4b5
VikashMahto01 <143506990+VikashMahto01@users.noreply.github.com>
2023-11-11 00:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (19.58%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n == 0 ){
+            return 0;
+        } else if(n == 1 || n == 2){
+            return 1;
+        } else {
+
+            int firstTerm = 0;
+            int secTerm = 1;
+            int thirdTerm = 1;
+
+            for(int i=3; i<=n; i++){
+
+                int fourthTerm = firstTerm + secTerm + thirdTerm;
+
+                firstTerm = secTerm;
+                secTerm = thirdTerm;
+                thirdTerm = fourthTerm;
+            }
+
+            return thirdTerm;
+        }
+    }
+};
